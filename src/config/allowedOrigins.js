2b9778c75583a12752b29e397eb6bf1a50ba71a7
succ-cloud <successbongnwi416@gmail.com>
2026-03-20 15:21:45 +0100

    Add localhost:3001 to allowed origins

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins=
     // 'https://www.google .com', 
         'https://wacheit.netlify.app',
         'http://localhost:5173',
+        'http://localhost:3001',
         'https://routestest.netlify.app/',
         'http://localhost:3000'
 
